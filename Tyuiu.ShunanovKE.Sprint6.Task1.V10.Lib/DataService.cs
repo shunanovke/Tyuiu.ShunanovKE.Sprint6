13cb00d98f8357548d4a1f4acaf32f4253a29fe5
shunanovke <shunan05@gmail.com>
2023-12-02 23:28:36 +0500

    Task0

---
+++ Tyuiu.ShunanovKE.Sprint6.Task1.V10.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.ShunanovKE.Sprint6.Task1.V10.Lib
+{
+    public class DataService
+    {
+    }
+}
---
+++ Tyuiu.ShunanovKE.Sprint6.Task1.V10.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShunanovKE.Sprint6.Task1.V10.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Tyuiu.ShunanovKE.Sprint6.Task1.V10.Test")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f7e764a3-d564-423d-9b39-72cd20704a06")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
